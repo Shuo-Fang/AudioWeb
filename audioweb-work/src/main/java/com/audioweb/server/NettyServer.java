4957903644792cbcc00572583613e209303a524e
hengyu zhu <hengyu.zhu@chinacreator.com>
2020-03-09 17:27:49 +0800

    no message

--- audioweb-work/src/main/java/com/audioweb/server/NettyServer.java
+++ audioweb-work/src/main/java/com/audioweb/server/NettyServer.java
@@ -25,6 +25,7 @@ import com.audioweb.server.handler.TcpServerHandler;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -114,9 +115,19 @@ public class NettyServer {
             EventLoopGroup tcpBossGroup = new NioEventLoopGroup(1);
             b.group(tcpBossGroup, tcpWorkerGroup)
                     .channel(islinux != null && islinux.equals("1")?EpollServerSocketChannel.class:NioServerSocketChannel.class)
-                    .option(ChannelOption.TCP_NODELAY, true)
                     .option(ChannelOption.SO_REUSEADDR, true)
+                    .option(ChannelOption.SO_BACKLOG,64)
+                    // 第2次握手服务端向客户端发送请求确认，同时把此连接放入队列A中，
+                    // 然后客户端接受到服务端返回的请求后，再次向服务端发送请求，表示准备完毕，此时服务端收到请求，把这个连接从队列A移动到队列B中，
+                    // 此时A+B的总数，不能超过SO_BACKLOG的数值，满了之后无法建立新的TCP连接,2次握手后和3次握手后的总数
+                    // 当服务端从队列B中按照FIFO的原则获取到连接并且建立连接[ServerSocket.accept()]后，B中对应的连接会被移除，这样A+B的数值就会变小
+                    //此参数对于程序的连接数没影响，会影响正在准备建立连接的握手。
+                    .option(ChannelOption.SO_KEEPALIVE,true)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS,30000)//连接超时30000毫秒
+                    .option(ChannelOption.SO_TIMEOUT,5000)//输入流的read方法被阻塞时，接受数据的等待超时时间5000毫秒，抛出SocketException
                     .localAddress(new InetSocketAddress(Integer.parseInt(qtClientPort)))
+                    //child是在客户端连接connect之后处理的handler，不带child的是在客户端初始化时需要进行处理的
+                    .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)//缓冲池
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
