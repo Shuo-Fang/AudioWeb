abb12da846402fb6eea37eb5121074a7d24460e4
hengyu zhu <hengyu.zhu@chinacreator.com>
2020-03-10 17:51:13 +0800

    无

---
+++ audioweb-common/src/main/java/com/audioweb/common/utils/audio/Mp3Utils.java
@@ -0,0 +1,25 @@
+/**   
+ * @Title: Mp3Utils.java 
+ * @Package com.audioweb.common.utils.audio 
+ * @Description: MP3音频信息处理获取类
+ * @author ShuoFang hengyu.zhu@chinacreator.com 1015510750@qq.com
+ * @date 2020年3月10日 下午1:58:10 
+ * @version V1.0   
+ */ 
+package com.audioweb.common.utils.audio;
+
+/** 
+ * @ClassName: Mp3Utils 
+ * @Description: MP3音频信息处理获取类
+ * @author ShuoFang hengyu.zhu@chinacreator.com 1015510750@qq.com 
+ * @date 2020年3月10日 下午1:58:10  
+ */
+public class Mp3Utils {
+	/** 歌曲名称 */
+    private static final String SONG_NAME_KEY = "TIT2";
+    /** 歌手名称 */
+    private static final String ARTIST_KEY = "TPE1";
+    /** 歌曲专辑 */
+    private static final String ALBUM_KEY = "TALB";
+    
+}
