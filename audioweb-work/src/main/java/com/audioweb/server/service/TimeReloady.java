b8f74ffc0b72865a2b6f57da198e042d7ecf8fb6
hengyu zhu <hengyu.zhu@chinacreator.com>
2020-04-16 17:48:37 +0800

    代码规范

--- audioweb-work/src/main/java/com/audioweb/server/service/TimeReloady.java
+++ audioweb-work/src/main/java/com/audioweb/server/service/TimeReloady.java
@@ -5,10 +5,17 @@ import java.util.TimerTask;
 import com.audioweb.common.thread.manager.AsyncManager;
 import com.audioweb.common.utils.StringUtils;
 import com.audioweb.work.domain.WorkTerminal;
-
+/**
+ * 定时延时执行判断终端是否入组
+ * @ClassName: TimeReloady
+ * @Description: 定时延时执行判断终端是否入组
+ * @author ShuoFang hengyu.zhu@chinacreator.com 1015510750@qq.com 
+ * @date 2020年4月16日 下午5:40:57
+ */
 public class TimeReloady extends TimerTask{
 	private WorkTerminal tInfo = null;
-	private List<WorkTerminal> WorkTerminals = null;
+	private List<WorkTerminal> workTerminals = null;
+	
 	public TimeReloady(WorkTerminal tInfo) {
 		// TODO Auto-generated constructor stub
 		this.tInfo = tInfo;
@@ -16,18 +23,20 @@ public class TimeReloady extends TimerTask{
 			AsyncManager.me().execute(this, tInfo.getRetry()*1000);
 		}
 	}
-	public TimeReloady(List<WorkTerminal> WorkTerminals) {
+	
+	public TimeReloady(List<WorkTerminal> workTerminals) {
 		// TODO Auto-generated constructor stub
-		this.WorkTerminals = WorkTerminals;
+		this.workTerminals = workTerminals;
 		AsyncManager.me().execute(this, 1000);
 	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		if(tInfo != null)
-			tInfoisCastCMDReturn(tInfo);
-		else if(WorkTerminals != null) {
-			isCastCMDReturn();
+		if (tInfo != null){
+			infoIsCastReturn(tInfo);
+		} else if(workTerminals != null){
+			isCastReturn();
 		}
 	}
 	
@@ -37,9 +46,9 @@ public class TimeReloady extends TimerTask{
 	 * TODO	监测终端是否入组(分组)
 	 * 时间：2019年1月11日
 	 */
-	private void isCastCMDReturn() {
-		for(WorkTerminal tInfo:WorkTerminals) {
-			tInfoisCastCMDReturn(tInfo);
+	private void isCastReturn() {
+		for(WorkTerminal tInfo:workTerminals) {
+			infoIsCastReturn(tInfo);
 		}
 	}
 	
@@ -48,7 +57,7 @@ public class TimeReloady extends TimerTask{
 	 * TODO	监测终端是否入组（单一终端）
 	 * 时间：2019年1月11日
 	 */
-	private void tInfoisCastCMDReturn(WorkTerminal tInfo) {
+	private void infoIsCastReturn(WorkTerminal tInfo) {
 		try {
 			if(StringUtils.isNotNull(tInfo.getCastTask()) && tInfo.getIsOnline() < 2 && tInfo.getRetry() > 0) {
 				//重新对指定终端发送入组命令
